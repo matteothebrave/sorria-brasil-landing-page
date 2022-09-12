dcd7f75ee316656208a90f7693e05443b44a325a
Matteo <thebravematteo33@protonmail.com>
2022-09-12 01:15:35 -0300

    fix/sorria-brasil-landing-page/fixing some issues on JavaScript and CSS

--- main.js
+++ main.js
@@ -1,78 +1,74 @@
 window.addEventListener('scroll', onScroll)
 
 onScroll()
-function onScroll() {
-  showNavOnScroll()
-  showBackToTopButtonOnScroll()
 
-  activateMenuAtCurrentSection(home)
-  activateMenuAtCurrentSection(services)
-  activateMenuAtCurrentSection(about)
-  activateMenuAtCurrentSection(contact)
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
 }
 
 function activateMenuAtCurrentSection(section) {
-  const targetLine = scrollY + innerHeight / 2
+    const targetLine = scrollY + innerHeight / 2
 
-  // verificar se a seção passou da linha
-  // quais dados vou precisar?
-  const sectionTop = section.offsetTop
-  const sectionHeight = section.offsetHeight
-  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
 
-  // verificar se a base está abaixo da linha alvo
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
 
-  const sectionEndsAt = sectionTop + sectionHeight
-  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+    const sectionEndsAt = sectionTop + sectionHeight
 
-  // limites da seção
-  const sectionBoundaries =
-    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
 
-  const sectionId = section.getAttribute('id')
-  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
 
-  menuElement.classList.remove('active')
-  if (sectionBoundaries) {
-    menuElement.classList.add('active')
-  }
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll() {
-  if (scrollY > 0) {
-    navigation.classList.add('scroll')
-  } else {
-    navigation.classList.remove('scroll')
-  }
+    if (scrollY > 0) {
+        navigation.classList.add('scroll')
+    } else {
+        navigation.classList.remove('scroll')
+    }
 }
 
 function showBackToTopButtonOnScroll() {
-  if (scrollY > 550) {
-    backToTopButton.classList.add('show')
-  } else {
-    backToTopButton.classList.remove('show')
-  }
+    if (scrollY > 500) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
 }
 
 function openMenu() {
-  document.body.classList.add('menu-expanded')
+    document.body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded')
+    document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700
+    origin: 'top',
+    distance: '30px',
+    duration: 700
 }).reveal(`
-  #home, 
-  #home img, 
-  #home .stats, 
-  #services,
-  #services header,
-  #services .card
-  #about, 
-  #about header, 
-  #about .content`)
+    #home, 
+    #home img, 
+    #home .stats, 
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about .content
+`);
